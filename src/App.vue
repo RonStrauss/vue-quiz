412dd477e460e6041eb72c10da7388b8475e1ffb
RonStrauss <ashbelthor@gmail.com>
2024-12-24 21:48:24 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const currentPage: '0' | '1' | '2' | '3' | '4' = '0'
+import FirstPage from './components/FirstPage.vue'
+</script>
+
+<template>
+  <header>החידה הגדולה</header>
+
+  <main>
+    <FirstPage v-if="currentPage === '0'" />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/FirstPage.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+  alert('Hello from FirstPage')
+</script>
+
+<template>
+  <div>
+    <h1>First Page</h1>
+  </div>
+</template>
