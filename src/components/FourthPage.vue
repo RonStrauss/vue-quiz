417962647292fd5bb21bdfebd1d886d3756eddcf
RonStrauss <ashbelthor@gmail.com>
2024-12-28 10:55:26 +0200

    Refactor FourthPage component to implement color selection and page transition logic

--- src/components/FourthPage.vue
+++ src/components/FourthPage.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import type { Icons } from '@/types/Icons'
 import CustomIconSvg from './CustomIconSvg.vue'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
 const emit = defineEmits(['changePage'])
 function changePage() {
@@ -9,7 +9,128 @@ function changePage() {
 }
 
 const expectedColor = '#FD5C4E'
+const selectedColor = ref('')
+const selectedAnswers = ref(Array(questions.length).fill(null))
+watch(selectedColor, (newVal) => {
+  if (newVal.toLowerCase() === expectedColor.toLowerCase()) {
+    changePage()
+  }
+})
+</script>
 
+<template>
+  <div class="flex">
+    <div class="flex-right">
+      <div class="question" v-for="(question, index) in questions" :key="index">
+        <p>{{ question.q }}</p>
+        <div class="buttons">
+          <label>
+            <input
+              type="radio"
+              :name="'question' + index"
+              value="true"
+              v-model="selectedAnswers[index]"
+            />
+            נכון
+          </label>
+          <label>
+            <input
+              type="radio"
+              :name="'question' + index"
+              value="false"
+              v-model="selectedAnswers[index]"
+            />
+            לא נכון
+          </label>
+          <CustomIconSvg
+            v-if="selectedAnswers[index] !== null"
+            :src="selectedAnswers[index] === 'true' ? question.t : question.f"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div class="flex-left">
+      <h2>מקרא</h2>
+      <div class="legend">
+        <div v-for="icon in legendIcons" :key="icon.code" class="legend-item">
+          <span>{{ icon.code }}</span>
+          <CustomIconSvg :src="icon.icon" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="answer">
+    <input type="color" v-model="selectedColor" />
+  </div>
+</template>
+
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+.flex-right {
+  display: grid;
+  gap: 1rem;
+}
+.answer input {
+  width: 150px;
+  height: 75px;
+}
+.flex-left {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+.question .buttons {
+  display: flex;
+  gap: 1rem;
+  min-height: 50px;
+}
+.left {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+.right {
+  display: flex;
+  gap: 0.5rem;
+}
+label {
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+  background-color: #f0f0f0;
+  cursor: pointer;
+  border: 1px solid var(--color-border);
+  outline: none;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  transition: 0.3s;
+}
+button:hover {
+  background-color: #e0e0e0;
+}
+
+.legend {
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(4, 1fr);
+  direction: ltr;
+}
+.legend-item {
+  display: flex;
+  flex-direction: column;
+  font-size: large;
+}
+
+.legend-item span {
+  font-weight: 500;
+  text-decoration: underline;
+}
+</style>
+
+<script lang="ts">
 const legendIcons: Array<{ code: string; icon: Icons }> = [
   {
     code: '0',
@@ -76,10 +197,13 @@ const legendIcons: Array<{ code: string; icon: Icons }> = [
     icon: 'filter',
   },
 ]
-
 const questions: Array<{ q: string; t: Icons; f: Icons }> = [
   { q: 'הלקוח הכי גדול של BMC Helix רכש 620k execitions', t: 'temperature-full', f: 'filter' },
-  { q: 'השנים שניר עובד ב BMC זהות לשנים שמעיין קנטור חיה', t: 'cruzeiro-sign', f: 'hurricane-alt' },
+  {
+    q: 'השנים שניר עובד ב BMC זהות לשנים שמעיין קנטור חיה',
+    t: 'cruzeiro-sign',
+    f: 'hurricane-alt',
+  },
   { q: 'רן הוא העובד הכי חדש בקבוצה', t: 'hashtag', f: 'overline' },
   { q: 'כיום יש x פלטפורמות בכל חשבונות פרודקשן', t: 'clock-eight', f: 'cube-alt-2' },
   {
@@ -89,117 +213,4 @@ const questions: Array<{ q: string; t: Icons; f: Icons }> = [
   },
   { q: 'חברת BMC פועלת במעל ל 50 מדינות בעולם ', t: 'cube-alt-2', f: 'hourglass' },
 ]
-
-const selectedAnswers = ref(Array(questions.length).fill(null))
 </script>
-
-<template>
-  <div class="flex">
-    <div class="flex-right">
-      <div class="question" v-for="(question, index) in questions" :key="index">
-        <p>{{ question.q }}</p>
-        <div class="buttons">
-          <label>
-            <input
-              type="radio"
-              :name="'question' + index"
-              value="true"
-              v-model="selectedAnswers[index]"
-            />
-            נכון
-          </label>
-          <label>
-            <input
-              type="radio"
-              :name="'question' + index"
-              value="false"
-              v-model="selectedAnswers[index]"
-            />
-            לא נכון
-          </label>
-          <CustomIconSvg
-            v-if="selectedAnswers[index] !== null"
-            :src="selectedAnswers[index] === 'true' ? question.t : question.f"
-          />
-        </div>
-      </div>
-    </div>
-
-    <div class="flex-left">
-      <h2>מקרא</h2>
-      <div class="legend">
-        <div v-for="icon in legendIcons" :key="icon.code" class="legend-item">
-          <span>{{ icon.code }}</span>
-          <CustomIconSvg :src="icon.icon" />
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="answer">
-    <input type="color" />
-  </div>
-</template>
-
-<style scoped>
-.flex {
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-}
-.flex-right {
-  display: grid;
-  gap: 1rem;
-}
-.answer input {
-  width: 150px;
-  height: 75px;
-}
-.flex-left {
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-}
-.question .buttons {
-  display: flex;
-  gap: 1rem;
-}
-.left {
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-}
-.right {
-  display: flex;
-  gap: 0.5rem;
-}
-label {
-  padding: 0.5rem 1rem;
-  border-radius: 0.5rem;
-  background-color: #f0f0f0;
-  cursor: pointer;
-  border: 1px solid var(--color-border);
-  outline: none;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  transition: 0.3s;
-}
-button:hover {
-  background-color: #e0e0e0;
-}
-
-.legend {
-  display: grid;
-  gap: 1rem;
-  grid-template-columns: repeat(4, 1fr);
-  direction: ltr;
-}
-.legend-item {
-  display: flex;
-  flex-direction: column;
-  font-size: large;
-}
-
-.legend-item span {
-  font-weight: 500;
-  text-decoration: underline;
-}
-</style>
